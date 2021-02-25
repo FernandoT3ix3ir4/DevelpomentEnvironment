c06ae0304675ef0db8d003a35e099fd6884b2a0c
FERNANDO TEIXEIRA SILVA <40283818+FernandoT3ix3ir4@users.noreply.github.com>
2021-02-25 15:48:43 -0300

    Develop (#12)
    * mergeback
    
    * inclusao do postman nos softwares instalados.
    
    * Update README.md
    
    * inclusao do github api cli
    
    * troca de jdk para openjdk e inclusão do refreshenv antes de usar o npm. (#6)
    
    * Atualizacao das ferramentas (#8)
    
    * Update README.md
    
    * Update README.md
    
    * Fernando t3ix3ir4 patch 1 (#11)
    
    * inclusão do windows Terminal
    
    * Inclusão do windows terminal
    
    * Inclusão do windows terminal

--- src/deleteDevelopmentEnvironment.ps1
+++ src/deleteDevelopmentEnvironment.ps1
@@ -62,7 +62,8 @@ function destroyDevelopmentEnvironment () {
     npm uninstall @angular/cli;
     Write-Information "Angular CLI desinstalado com sucesso!";
     
-    $tools = @( "openjdk8", "maven", "nodejs-lts", "git", "dotnetcore-sdk", "dart-sdk", "flutter", "vscode", "AndroidStudio", "android-sdk", "docker", "postman", "gh");
+    $tools = @( "openjdk8", "maven", "nodejs-lts", "git", "dotnetcore-sdk", "dart-sdk", "flutter", "vscode", "AndroidStudio", "android-sdk", "docker", "postman", "gh", "microsoft-windows-terminal");
+
     foreach ($tool in $tools) {
         choco uninstall $tool -y --accept-license -f;
         Write-Information "${tool} desinstalado com sucesso!";
